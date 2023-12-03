58d1d70b915d7bc56cfdb16c46e69f845f0d327b
BryCur <bryan.curchod@gmail.com>
2023-12-03 13:31:32 +0100

    day 3- complete

--- day-3/main.py
+++ day-3/main.py
@@ -1,5 +1,7 @@
 from utils import *
 
+PUZZLE_PART = 2
+
 matrix = []
 
 with open("day-3/input.txt") as f:
@@ -16,8 +18,18 @@ while True:
 
     numberPositions = getAdjacentNumberPositionsFrom(matrix, currX, currY)
 
-    for pos in numberPositions:
-            totalValue += consumeNumberAtPosition(matrix, *pos)
+    if PUZZLE_PART == 1:
+        for pos in numberPositions:
+                totalValue += consumeAndReturnNumberAtPosition(matrix, *pos)
+    if PUZZLE_PART == 2:
+         if countAllUniqueNumberAroundPos(matrix, currX, currY) == 2 and matrix[currX][currY] == "*":
+                product = 1
+                for pos in numberPositions:
+                    number_at_pos = consumeAndReturnNumberAtPosition(matrix, *pos)
+                    product *= number_at_pos if number_at_pos != 0 else 1
+                totalValue += product
+
+
 
     
     currY += 1
