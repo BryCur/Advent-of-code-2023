b3011f9433a1bd76495d32be881faa4ed3f087ac
Bryan Curchod <bryan.curchod@gmail.com>
2023-12-08 16:44:16 +0100

    day 8 - part 2 - not working

--- day-8/main.py
+++ day-8/main.py
@@ -4,6 +4,8 @@ import functools
 directionSequence: str
 nodeMap : dict = {}
 
+PART_SELECTOR = 2
+
 with open("day-8/input.txt") as f:
     lines = f.readlines()
     for line in lines:
@@ -14,12 +16,30 @@ with open("day-8/input.txt") as f:
             else: 
                 directionSequence = line.strip()
 
-startingNode = "AAA"
-step_count = 0
-currentNode = startingNode
-while currentNode != "ZZZ":
-    currentNode = getNextStep(nodeMap, currentNode, directionSequence[step_count%len(directionSequence)])
-    step_count += 1
-    #print(currentNode)
+if PART_SELECTOR == 1:
+    startingNode = "AAA"
+    step_count = 0
+    currentNode = startingNode
+    while currentNode != "ZZZ":
+        currentNode = getNextStep(nodeMap, currentNode, directionSequence[step_count%len(directionSequence)])
+        step_count += 1
+        #print(currentNode)
 
-print(f"Part 1 - step from AAA to ZZZ: {step_count}")
+    print(f"Part 1 - step from AAA to ZZZ: {step_count}")
+
+else: 
+    startingNodes = [ key for key in nodeMap.keys() if key[-1] == 'A']
+    step_count = 0
+    currentNodes = startingNodes
+
+    print(currentNodes)
+    
+    while shouldContinue(currentNodes):
+        nextNodes = []
+        for node in currentNodes:
+            nextNodes.append(getNextStep(nodeMap, node, directionSequence[step_count%len(directionSequence)]))
+        
+        step_count += 1
+        currentNodes = nextNodes
+
+    print(f"Part 2 - step from AAA to ZZZ: {step_count}")
