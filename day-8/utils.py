0df034f56f68a4bf624a74d42c1b772a903b2f0f
BryCur <bryan.curchod@gmail.com>
2023-12-08 18:52:12 +0100

    day 8 - part 2 OK

--- day-8/utils.py
+++ day-8/utils.py
@@ -13,3 +13,22 @@ def shouldContinue(nodes: list[str]) -> bool:
             return True
         
     return False
+
+
+class Arrival:
+    arrivalNode: str
+    totalStepRequired: int
+    relativeStepRequired: int
+    positionInSequence: int
+
+    def __init__(self, node: str, total_step: int, previous_arrival_steps: int, sequence_position: int) -> None:
+        self.arrivalNode= node
+        self.totalStepRequired = total_step
+        self.relativeStepRequired = total_step - previous_arrival_steps
+        self.positionInSequence = sequence_position
+        pass
+
+    def __str__(self):
+        return f"\nArrival(node: {self.arrivalNode}, totalstep: {self.totalStepRequired}, step from previous: {self.relativeStepRequired}, position in sequence: {self.positionInSequence})"
+    def __repr__(self):
+        return self.__str__()
