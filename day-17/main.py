a5d5b54e96366c2bb6a39fae2b37093147a1a8e5
Bryan Curchod <bryan.curchod@gmail.com>
2023-12-22 13:19:52 +0100

    day 17 - maybe it works but it might need a lot of time

--- day-17/main.py
+++ day-17/main.py
@@ -3,12 +3,50 @@ from utils import *
 matrix : list[list[int]] = []
 
 with open("day-17/example.txt") as file: 
-    for line in file:
+    firstLineLength: int = -1
+    for line in file.readlines():
         matrix.append(list(map(int, list(line.strip()))))
+        if firstLineLength == -1:
+            firstLineLength = len(matrix[-1])
+        else:
+            assert(firstLineLength == len(matrix[-1]))
 
 
 pretty_print_list(matrix)
 
+node_dict : dict[tuple, Node] = {}
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        currNode = Node((i,j), matrix[i][j])
+        if (i-1, j) in node_dict:
+            currNode.addNeighbour(Direction.UP, node_dict[(i-1, j)])
+            node_dict[(i-1, j)].addNeighbour(Direction.DOWN, currNode)
+        if (i+1, j) in node_dict:
+            currNode.addNeighbour(Direction.DOWN, node_dict[(i+1, j)])
+            node_dict[(i+1, j)].addNeighbour(Direction.UP, currNode)
+        if (i, j-1) in node_dict:
+            currNode.addNeighbour(Direction.LEFT, node_dict[(i, j-1)])
+            node_dict[(i, j-1)].addNeighbour(Direction.RIGHT, currNode)
+        if (i, j+1) in node_dict:
+            currNode.addNeighbour(Direction.RIGHT, node_dict[(i, j+1)])
+            node_dict[(i, j+1)].addNeighbour(Direction.LEFT, currNode)
+
+        node_dict[(i,j)] = currNode
+        
+
 start_pos = (0,0)
-final_pos = (len(matrix)-1, len(matrix[1]-1))
+final_pos = (len(matrix)-1, len(matrix[1])-1)
+
+
+starting_node = node_dict[start_pos]
+destination  = node_dict[final_pos]
+
+minHeatlossPath: list[tuple[int, int]] = None
+minHeatloss = -1
+
+minHeatloss = findPathToDestination(destination.position, starting_node, minHeatloss, [], 0, None, 0)
+
+print(f"testing a little; heatloss: {minHeatloss}, path {minHeatlossPath}")
+
 
--- day-17/utils.py
+++ day-17/utils.py
@@ -7,8 +7,62 @@ class Direction(Enum):
     UP = 2
     DOWN = 3
 
+class Node: 
+    position: tuple
+    neighbours: dict[Direction, 'Node']
+    visited: bool = False
+    heatLoss: int = 0
+
+    def __init__(self, position: tuple, value: int, nLeft = None, nRight = None, nUp = None, nDown = None) -> None:
+
+        self.position = position
+
+        self.neighbours = dict()
+        self.neighbours[Direction.LEFT] = nLeft
+        self.neighbours[Direction.RIGHT] = nRight
+        self.neighbours[Direction.UP] = nUp
+        self.neighbours[Direction.DOWN] = nDown
+
+        self.visited = False
+        self.heatLoss = value
+
+    def setVisited(self, newval: bool) -> None:
+        self.visited = newval
+
+    def getNeighbourAtDirection(self, direction: Direction): 
+        return self.neighbours[direction]
+    
+    def isVisited(self) -> bool: 
+        return self.visited
+    
+    def addNeighbour(self, direction: Direction, value): 
+        self.neighbours[direction] = value
+
+
 
 def pretty_print_list(pList: list[str]):
     for elem in pList:
         print(elem)
     print("-" * 50)
+
+
+def findPathToDestination(destination: tuple[int, int], currentNode: Node, minheatLoss: int, currentPath: list, currentHeatLoss: int, lastDirection: Direction, sameDirectionCount: int):
+    if currentNode.position == destination:
+        # print(f"destination reached, minHeatloss: {minheatLoss}, current heatloss: {currentHeatLoss}, path: {currentPath}")
+        if minheatLoss == -1 or minheatLoss > currentHeatLoss:
+            minheatLoss = currentHeatLoss
+        return minheatLoss
+    
+    
+    currentNode.setVisited(True)
+    currentPath.append(currentNode.position)
+
+
+    for key, val in currentNode.neighbours.items():
+        if val is not None and not val.isVisited() and (key != lastDirection or sameDirectionCount < 3):
+            minheatLoss = findPathToDestination(destination, val, minheatLoss, currentPath, (currentHeatLoss + val.heatLoss), key, sameDirectionCount +1 if key == lastDirection else 0)
+
+    currentNode.setVisited(False)
+    currentPath.pop()
+
+    return minheatLoss
